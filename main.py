4bfd69dfe72f7912feb97dd36f5893a903a61c35
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-08-06 21:57:59 +0430

    Change some lines

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from random import choice
 
+computer_points, user_points = 0, 0
+
 actions = ["rock", "paper", "scissors", "lizard", "spock"]
 
+computer_action = choice(actions)
 user_input = str(input("What is your act (rock, paper, scissors, lizard, spock): "))
 
 if (user_input in actions):
     user_action = user_input
-
-computer_action = choice(actions)
-
-computer_points = 0
-user_points = 0
 
 def check_actions(computer_act, user_act):
     return True
