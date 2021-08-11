107a07fda8d8fc80ea933b57f51cb6c57ea9910e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-08-11 09:41:22 +0430

    Create if user show rock

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from random import choice
 
-computer_points, user_points = 0, 0
-
 actions = ["rock", "paper", "scissors", "lizard", "spock"]
 
 computer_action = choice(actions)
@@ -11,4 +9,20 @@ if (user_input in actions):
     user_action = user_input
 
 def check_actions(computer_act, user_act):
-    return True
+    computer_points, user_points = 0, 0
+
+    if user_act == actions[0]:
+        if computer_act == actions[0]:
+            print("Both same.")
+        elif computer_act == actions[1]:
+            print("Paper Covers Rock.")
+            computer_points = computer_points + 1
+        elif computer_act == actions[2]:
+            print("Rock crushes Scissors.")
+            user_points = user_points + 1
+        elif computer_act == actions[3]:
+            print("Rock crushes Lizard.")
+            user_points = user_points + 1
+        elif computer_act == actions[4]:
+            print("Spock smashes Scissors.")
+            computer_points = computer_points + 1
